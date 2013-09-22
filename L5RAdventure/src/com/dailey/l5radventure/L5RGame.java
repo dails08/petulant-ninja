f57e9c952baccad6b5177c201671485904a452be
Chris Dailey <christopher.dailey@gmail.com>
2013-09-21 22:23:13 -0400

    Refactored the stone presentation code a bit.

--- L5RAdventure/src/com/dailey/l5radventure/L5RGame.java
+++ L5RAdventure/src/com/dailey/l5radventure/L5RGame.java
@@ -1,14 +1,6 @@
 package com.dailey.l5radventure;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class L5RGame extends Game{
 	
