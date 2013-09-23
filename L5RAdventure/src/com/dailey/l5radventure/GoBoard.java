400a00125ec171842f1f5cd1d67ba28ca7489415
Chris Dailey <christopher.dailey@gmail.com>
2013-09-23 12:38:45 -0400

    Working on proceeding with game, list_stones looks much easier than parsing through showboard.

--- L5RAdventure/src/com/dailey/l5radventure/GoBoard.java
+++ L5RAdventure/src/com/dailey/l5radventure/GoBoard.java
@@ -1,5 +1,10 @@
 package com.dailey.l5radventure;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -10,39 +15,53 @@ public class GoBoard implements InputProcessor{
 	
 	GoGame game;
 	Sprite goban, whitestone, blackstone;
+	OutputStreamWriter pInput;
+	ArrayBlockingQueue<String> messages;
 	float x,y, width, height, scale, pX, pY;
 	
 
 	public void draw(SpriteBatch batch)
 	{
 		goban.draw(batch);
+		float divisionWidth = getWidth()/19;
+		float divisionHeight = getHeight()/19;
+		for (int i = 0; i < 19;i++)
+		{
+			for (int j = 0; j < 19; j++)
+			{
+				if (game.getCoord(i, j)==1)
+				{
+					blackstone.setPosition(i*divisionHeight+divisionHeight/2, j*divisionWidth+divisionWidth/2);
+					blackstone.draw(batch);
+				}
+				else if (game.getCoord(i, j)==2)
+				{
+					whitestone.setPosition(i*divisionHeight+divisionHeight/2, j*divisionWidth+divisionWidth/2);
+					whitestone.draw(batch);
+				}
+				else
+					assert game.getCoord(i, j)==0;
+					
+			}
+		}
 		if (goban.getBoundingRectangle().contains(pX, pY))
 		{
+			blackstone.setPosition((float)(Math.floor(pX/divisionWidth)*divisionWidth), (float)(Math.floor(pY/divisionHeight)*divisionHeight));	
+
 			if (game.blacksTurn)
-			{
-				
-				//blackstone.setPosition(pX-blackstone.getWidth()/2, pY-blackstone.getHeight()/2);
-				//float stoneX = pX-blackstone.getWidth()/2;
-				//float stoneY = pY-blackstone.getHeight()/2;
-				//blackstone.setPosition(stoneX, stoneY);
-				float divisionWidth = getWidth()/19;
-				float divisionHeight = getHeight()/19;
-				blackstone.setPosition((float)(Math.floor(pX/divisionWidth)*divisionWidth), (float)(Math.floor(pY/divisionHeight)*divisionHeight));
 				blackstone.draw(batch);
-			}
 			else
 			{
 				assert !game.blacksTurn;
-				whitestone.setPosition(pX, pY);
 				whitestone.draw(batch);
-	
 			}
 		}
+
 		
 		
 	}
 	
-	public GoBoard(TextureAtlas goAtlas)
+	public GoBoard(TextureAtlas goAtlas, OutputStreamWriter pInput, ArrayBlockingQueue<String> messages)
 	{
 		x=0;
 		y=0;
@@ -53,6 +72,8 @@ public class GoBoard implements InputProcessor{
 		scale = 1.0f;
 		goban = goAtlas.createSprite("goban");
 		goban.setSize(getWidth(),getHeight());
+		this.pInput = pInput;
+		this.messages = messages;
 		blackstone = goAtlas.createSprite("blackstone");
 		blackstone.setSize(stoneSize(), stoneSize());
 		blackstone.setOrigin(blackstone.getWidth()/2, blackstone.getHeight()/2);
@@ -127,9 +148,60 @@ public class GoBoard implements InputProcessor{
 	}
 
 	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) 
+	{
+		if (game.blacksTurn)
+		{
+			int tempX = (int)(blackstone.getX()/(getWidth()/19));
+			char transX = (char) (tempX+65);
+			int tempY = (int)(blackstone.getY()/(getHeight()/19)) + 1;
+			try {
+				String command = "play black "+transX+""+tempY+"\n";
+				Gdx.app.log("GoBoard", "Sending to goEngine: "+command);
+				pInput.write(command);
+				pInput.flush();
+				if (getNext().contains("="))
+				{
+					Gdx.app.log("GoBoard", "Command accepted");
+					Gdx.app.log("GoBoard", "Sending to goEngine: genmove white");
+					pInput.write("genmove white\n");
+					pInput.flush();
+					if (getNext().contains("="))
+					{
+						Gdx.app.log("GoBoard", "Sending to goEngine: showboard");
+						pInput.write("showboard\n");
+						pInput.flush();
+						if (getNext().equals("= "))
+						{
+							Gdx.app.log("GoBoard", "Updating board");
+							game.updateBoard(messages);
+						}
+					}
+
+
+				}
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 		return false;
+	}
+	
+	private String getNext() throws InterruptedException
+	{
+		String got = messages.poll(2, TimeUnit.SECONDS);
+		while (got.equals("\n") || got.equals(""))
+		{
+			got = messages.poll(2, TimeUnit.SECONDS);
+		}
+		Gdx.app.log("GetNext", "Returning \""+got+"\"");
+		return got;
 	}
 
 	@Override
